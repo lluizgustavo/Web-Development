3b649708c29b6f6247a7420a26103f6fde7e8ec4
Luiz <luizsilvagf98@gmail.com>
2026-07-19 00:10:01 -0300

    Another exception flow exercise - customer request

---
+++ fundamentals/exception-flow-practices/ex03/customer-request.js
@@ -0,0 +1,154 @@
+const clients = [
+  {
+    name: 'João da Silva',
+    age: 28,
+    email: 'joao.silva@gmail.com',
+    phone: '(11) 5555-5555',
+    address: {
+      street: 'Rua dos Lírios',
+      number: 123,
+      neighborhood: 'Jardim Primavera',
+      city: 'São Paulo',
+      state: 'SP',
+      cep: '01234-567',
+    },
+  },
+  {
+    name: 'Maria Souza',
+    age: 35,
+    email: 'maria.souza@hotmail.com',
+    address: {
+      street: 'Rua dos Cravos',
+      number: 456,
+      neighborhood: 'Copacabana',
+      city: 'Rio de Janeiro',
+      state: 'RJ',
+      cep: '12345-678',
+    },
+  },
+  {
+    name: 'Pedro Oliveira',
+    age: 42,
+    email: 'pedro.oliveira@gmail.com',
+    address: {
+      street: 'Rua das Margaridas',
+      number: 789,
+      neighborhood: 'Boa Viagem',
+      city: 'Recife',
+      state: 'PE',
+      cep: '23456-789',
+    },
+  },
+  {
+    name: 'Ana Santos',
+    age: 25,
+    email: 'ana.santos@gmail.com',
+    phone: '(71) 5555-5555',
+    address: {
+      street: 'Rua dos Girassóis',
+      number: 1011,
+      neighborhood: 'Barra',
+      city: 'Salvador',
+      state: 'BA',
+      cep: '34567-890',
+    },
+  },
+  {
+    name: 'Luiz Costa',
+    age: 32,
+    email: 'luiz.costa@hotmail.com',
+    address: {
+      street: 'Rua das Acácias',
+      number: 1213,
+      neighborhood: 'Centro',
+      city: 'Belo Horizonte',
+      state: 'MG',
+      cep: '45678-901',
+    },
+  },
+  {
+    name: 'Isabela Almeida',
+    age: 37,
+    email: 'isabela.almeida@gmail.com',
+    phone: '(21) 5555-5555',
+    address: {
+      street: 'Rua das Hortênsias',
+      number: 1415,
+      neighborhood: 'Botafogo',
+      city: 'Rio de Janeiro',
+      state: 'RJ',
+      cep: '56789-012',
+    },
+  },
+  {
+    name: 'Rafael Ferreira',
+    age: 29,
+    email: 'rafael.ferreira@hotmail.com',
+    address: {
+      street: 'Rua das Orquídeas',
+      number: 1617,
+      neighborhood: 'Pinheiros',
+      city: 'São Paulo',
+      state: 'SP',
+      cep: '67890-123',
+    },
+  },
+];
+
+const findPersonByName = (name) => {
+    try {
+        for(let index = 0; index < clients.length; index +=1){
+            if (clients[index].name === name) {
+                return `Destinatário: ${clients[index].name}, Endereço: ${clients[index].address.street},
+                ${clients[index].address.number}, ${clients[index].address.neighborhood}, ${clients[index].address.city}
+                ${clients[index].address.state}, CEP: ${clients[index].address.cep}`
+            }
+        }
+        throw new Error("Pessoa não encontrada, tente novamente.")
+    }
+    
+    catch (error) {
+        return error.message;
+    }
+
+};
+
+const findPersonByPosition = (position) => {
+    try {
+        if (position >= 0 && position <= 6) {
+            const client = clients[position]
+            return `Destinatário: ${client.name}, Endereço: ${client.address.street},
+                ${client.address.number}, ${client.address.neighborhood}, ${client.address.city}
+                ${client.address.state}, CEP: ${client.address.cep}`
+        }
+        throw new Error("Posição inválida, tente novamente.")
+    }
+    catch (error) {
+        return error.message;
+    }
+};
+
+const findPeopleByState = (state) => {
+    try {
+    let clientsState = []
+    for (let index = 0; index < clients.length; index +=1) {
+        if (clients[index].address.state === state) {
+            clientsState.push(clients[index].name)
+        }
+    }
+    if (clientsState.length === 0) {
+    throw new Error("Ops, nenhuma pessoa mora nesse estado, tente outro.")
+        }
+
+    return clientsState;
+    }
+    catch (error) {
+    return error.message;
+  }
+    
+};
+
+
+// console.log(findPersonByName("Luiz Costa"))
+// console.log(findPersonByPosition(8))
+console.log(findPeopleByState("RJ"))
